7d767db590ef95cd0e67ab98cc103d6787fe17e0
Firastic <adyaksa.wisanggeni@gmail.com>
2018-11-10 16:16:07 +0700

    Update format

--- main.py
+++ main.py
@@ -4,8 +4,10 @@
 # Receive Operation
 # Update display
 
+from __future__ import print_function
 import OpenGL
 import random
+from transformation import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
 from OpenGL.GLUT import *
@@ -21,18 +23,11 @@ lx = 0.0
 lz = -1.0
 x = 0.0
 z = 5.0
+currentCommand = ""
 
-def displayObject2D():
+def displayObject():
 	#Menampilkan object saat ini
-    glColor3f(random.random(),random.random(),random.random())
-    glBegin(GL_POLYGON)
-    for edge in edges:
-        for vertex in edge:
-            glVertex3fv(vertices[vertex])
-    glEnd()
-
-def displayObject3D():
-	return
+	shape.output()
 
 def displayCartesian2D():
 	#Menampilkan sumbu x dan y
@@ -47,37 +42,67 @@ def displayCartesian2D():
 	glEnd()
 
 def displayCartesian3D():
+	#Todo
 	return
 
-
 def input2D():
-	is2D = True
+	global shape
 	N = int(raw_input("Masukkan nilai N\n"))
-	print "Masukkan " + str(N) + " buah titik 2 dimensi"
+	print("Masukkan " + str(N) + " buah titik 2 dimensi")
 	for i in range(N):
-		print i
 		x, y = map(float, raw_input().split())
 		z = 0
 		arr = [x/25,y/25,z]
-		print arr
 		vertices.insert(len(vertices), arr)
 		edges.insert(len(edges),[i,(i+1)%N])
+	shape = Object2D(vertices, edges)
 
 def input3D():
-	is3D = True
+	#Todo
+	return
+
+def inputDimensionChoice():
+	global is2D,is3D
+	x = int(input("Keluarkan 2 jika ingin 2 dimensi, dan 3 jika ingin 3D\n"))
+	while(x != 2 and x != 3):
+		print("Masukkan salah")
+		x = int(input("Keluarkan 2 jika ingin 2 dimensi, dan 3 jika ingin 3D\n"))
+	if(x == 2):
+		is2D = True
+	else:
+		is3D = True
+
+def keyPressed(key, x, y):
+	#Todo: Writing while displaying on terminal
+	global currentCommand
+	if(key == "\n"):
+		currentCommand = ""
+		return
+	elif(ord(key) == 8):
+		key = "\b \b"
+		currentCommand = currentCommand[:-1]
+	else:
+		currentCommand += key
+	print(key, end='')
+
+def openGLDisplay():
+	glutInit(sys.argv)
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
+	glutInitWindowSize(400,400)
+	glutCreateWindow(window_name)
+	glutDisplayFunc(display)
+	#glutReshapeFunc(changeSize)
+	glutKeyboardFunc(keyPressed)
+	glutMainLoop()
 	return
 
 def main():
-    input2D()
-    glutInit(sys.argv)
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
-    glutInitWindowSize(800,800)
-    glutCreateWindow(window_name)
-    glutDisplayFunc(display)
-
-    #glutReshapeFunc(changeSize)
-    glutMainLoop()
-    return
+	inputDimensionChoice()
+	if(is2D):		
+		input2D()
+	else:
+		input3D()
+	openGLDisplay()
 
 def display():
 	glClearColor(1.0, 1.0, 1.0, 0.0)
@@ -86,11 +111,11 @@ def display():
 	if(is2D):
 		gluLookAt(0.0, 0.0, 0.0, 0.0, 0.0, -1.0, 0.0, 1.0,  0.0);
 		displayCartesian2D()
-		displayObject2D()
+		displayObject()
 	else:
+		gluLookAt(0.0, 0.0, 0.0, 0.0, 0.0, -1.0, 0.0, 1.0,  0.0);
 		displayCartesian3D()
-		displayObject3D()
-
+		displayObject()
 	glPopMatrix()
 	glutSwapBuffers()
 	return
--- transformation.py
+++ transformation.py
@@ -1,52 +1,85 @@
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
+import random
+
 class Object:
 
-	initPoints = []
-	points = []
+	initVertices = []
+	vertices = []
+	edges = []
 
 	#Menginisialisasi Object
-	def __init__(self, points, edges):
-		self.points = points
-		self.initPoints = points
+	def __init__(self, vertices, edges):
+		self.vertices = vertices
+		self.initVertices = vertices
+		self.edges = edges
 
+	def output(self):
+		glColor3f(random.random(),random.random(),random.random())
+		glBegin(GL_POLYGON)
+		for edge in self.edges:
+			for vertex in edge:
+				glVertex3fv(self.vertices[vertex])
+		glEnd()
 
 class Object2D(Object):
 	def translate(self, dx, dy):
 		#Melakukan translasi tiap point sejauh dx,dy
+		return
 
 	def dilate(self, k):
 		#Melakukan dilatasi tiap point sebesar k
+		return
 
 	def rotate(self, deg, a, b):
 		#Melakukan rotasi tiap titik dengan deg derajat dengan titik pusat (a,b)
+		return
 
 	def reflect(self, param):
 		#Melakukan refleksi dengan parameter
+		return
 
 	def shear(self, param, k):
 		#Melakukan operasi shear pada objek
+		return
 
 	def stretch(self, param, k):
 		#Melakukan operasi stretch pada objek
+		return
 
 	def custom(self, a, b, c, d):
 		#Melakukan transformasi dengan matriks [[a,b][c,d]]
+		return
 
 	def multiple(self, n):
 		#Melakukan n buah transformasi
+		return
 
 class Object3D(Object):
 	def translate(self, dx, dy, dz):
+		return
 
 	def dilate(self, k):
+		return
 
 	def rotate(self, deg, a, b, c):
+		return
 
 	def reflect(self, param):
+		return
 
 	def shear(self, param, k):
+		return
 
 	def stretch(self, param, k):
+		return
 
 	def custom(self, a, b, c, d, e, f, g, h, i):
+		return
 
 	def multiple(self, n):
+		return
+
+	def output(self):
+		return
